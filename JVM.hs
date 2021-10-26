f982df87ac61e1ac9d64bac01c39d82c08a6f3b0
soczil <ksoczewica@gmail.com>
2021-10-26 15:41:16 +0200

    instant WIP

--- JVM.hs
+++ JVM.hs
@@ -1,2 +1,28 @@
-module JVM where
-    
+module Main where
+
+import System.IO (readFile)
+import System.Environment (getArgs)
+
+import Instant.Par
+import Instant.ErrM
+import Instant.Abs
+
+compile :: Program -> IO String
+compile = undefined 
+
+parseAndCompile :: String -> IO ()
+parseAndCompile instantProgram = do
+    case pProgram (myLexer instantProgram) of
+        Ok prog -> do
+            result <- compile prog
+            return ()
+        Bad msg -> putStrLn msg
+
+main :: IO ()
+main = do
+    args <- getArgs
+    case args of
+        [] -> putStrLn "No path to file"
+        (x:_) -> do
+            instantProgram <- readFile x
+            parseAndCompile instantProgram
