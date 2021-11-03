500d8babcc2063131d8c7ccc106e3d121a11dc9d
soczil <ksoczewica@gmail.com>
2021-11-03 12:38:51 +0100

    instant llvm WIP

--- LLVM.hs
+++ LLVM.hs
@@ -11,34 +11,69 @@ import Instant.Par
 import Instant.ErrM
 import Instant.Abs
 
-type LLVMMonad = State (S.Set Ident, Int)
+type LLVMState = (S.Set Ident, Int)
+type LLVMMonad = State LLVMState
 
-compileExp :: Exp -> LLVMMonad String
-compileExp (ExpAdd e1 e2) = undefined
-compileExp (ExpSub e1 e2) = undefined
-compileExp (ExpMul e1 e2) = undefined
-compileExp (ExpDiv e1 e2) = undefined
-compileExp (ExpLit id) = undefined
-compileExp (ExpVar id) = undefined
+emptyState :: LLVMState
+emptyState = (S.empty, 1)
+
+getFreeRegister :: LLVMMonad String
+getFreeRegister = do
+    (vars, counter) <- get
+    put (vars, counter + 1)
+    return $ "%t" ++ show counter
+
+register :: Int -> String
+register n = "%t" ++ show n
+
+loadInstr :: String -> Ident -> String
+loadInstr register (Ident id) = register ++ " = load i32, i32* %" ++ id ++ "\n"
+
+arithmeticInstr :: String -> String -> String -> String -> String
+arithmeticInstr register spot1 spot2 opCode = 
+    register ++ " = " ++ opCode ++ " i32 " ++ spot1 ++ ", " ++ spot2 ++ "\n"
+
+compileArithmeticExp :: Exp -> Exp -> String -> LLVMMonad (String, String)
+compileArithmeticExp e1 e2 opCode = do
+    (result1, spot1) <- compileExp e1
+    (result2, spot2) <- compileExp e2
+    register <- getFreeRegister
+    let instr = arithmeticInstr register spot1 spot2 opCode
+    return (result1 ++ result2 ++ instr, register)
+
+compileExp :: Exp -> LLVMMonad (String, String)
+compileExp (ExpAdd e1 e2) = compileArithmeticExp e1 e2 "add"
+compileExp (ExpSub e1 e2) = compileArithmeticExp e1 e2 "sub"
+compileExp (ExpMul e1 e2) = compileArithmeticExp e1 e2 "mul"
+compileExp (ExpDiv e1 e2) = compileArithmeticExp e1 e2 "udiv"
+compileExp (ExpLit n) = return ("", show n)
+compileExp (ExpVar id) = do
+    register <- getFreeRegister
+    return (loadInstr register id, register)
 
 compileStmt :: Stmt -> LLVMMonad String
 compileStmt (SAss id e) = undefined
-compileStmt (SExp e) = undefined
+compileStmt (SExp e) = do
+    (result, _) <- compileExp e
+    return result
 
 compileProgram :: [Stmt] -> LLVMMonad String
 compileProgram stmts = do
     result <- mapM compileStmt stmts
     return $ concat result
 
-compile :: Program -> String -> IO String
-compile = undefined
+compile :: Program -> IO String
+compile (Prog stmts) = do
+    let (compiledCode, _) = runState (compileProgram stmts) emptyState
+    return compiledCode
 
 runCompiler :: String -> IO ()
 runCompiler filePath = do
     instantProgram <- readFile filePath
     case pProgram (myLexer instantProgram) of
         Ok prog -> do
-            return ()
+            result <- compile prog
+            putStr result
         Bad msg -> hPutStrLn stderr $ "Error: " ++ msg
 
 main :: IO ()
