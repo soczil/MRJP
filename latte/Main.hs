64eaca6c679b7b505d053dd09931fa471b572aa1
soczil <ksoczewica@gmail.com>
2021-11-22 02:38:15 +0100

    latte front WIP

--- latte/Main.hs
+++ latte/Main.hs
@@ -2,9 +2,27 @@ module Main where
 
 import System.IO
 import System.Environment (getArgs)
+import System.Exit (ExitCode(ExitFailure), exitWith)
+
+import Latte.Par
+import Latte.ErrM
+
+import Typechecker
+
+finishTypechecker :: (String, Bool) -> IO ()
+finishTypechecker (_, False) = return ()
+finishTypechecker (msg, True) = do
+    hPutStrLn stderr msg
+    exitWith $ ExitFailure 1
 
 runCompiler :: String -> IO ()
-runCompiler filePath = undefined
+runCompiler filePath = do
+    latteProgram <- readFile filePath
+    case pProgram (myLexer latteProgram) of
+        Ok prog -> do
+            result <- check prog
+            finishTypechecker result
+        Bad msg -> hPutStrLn stderr msg
 
 main :: IO ()
 main = do
--- latte/Typechecker.hs
+++ latte/Typechecker.hs
@@ -1,3 +1,66 @@
 module Typechecker (check) where
 
-check = undefined
+import Control.Monad.Except
+import Control.Monad.State
+
+import Latte.Abs
+
+import qualified Data.Map as M
+import qualified Data.Set as S
+
+type TCEnv = M.Map Ident TCInf
+type TCOldVars = S.Set Ident
+type TCExcept = ExceptT TCError IO
+type TCState = (TCEnv, TCOldVars, Type)
+type TCMonad = StateT TCState TCExcept
+
+data TCInf = VarInf Type
+           | FunInf (Type, [Type]) 
+    deriving Eq
+
+data TCError = NoMainFunction
+             | FunAlreadyDeclared Ident BNFC'Position
+
+-- ============================ ERROR =================================
+
+errMsgPref :: BNFC'Position -> String
+errMsgPref p = case p of
+    Nothing -> "Static Error: "
+    Just (l, _) -> "Static Error at line " ++ show l ++ ": "
+
+showId :: Ident -> String
+showId (Ident id) = "[" ++ id ++ "]"
+
+errMsg :: TCError -> String
+errMsg (FunAlreadyDeclared id p) = errMsgPref p ++
+    "Function " ++ showId id ++ " already declared"
+
+-- ===================================================================
+
+emptyState :: TCState
+emptyState = (M.empty, S.empty, Void BNFC'NoPosition)
+
+funToEnv :: TopDef -> TCMonad ()
+funToEnv (FnDef p t id args _) = do
+    (env, oldVars, retType) <- get
+    case M.lookup id env of
+        Nothing -> do
+            let argTypeList = foldr (\(Arg _ t _) acc -> t : acc) [] args
+            put (M.insert id (FunInf (t, argTypeList)) env, oldVars, retType)
+        Just _ -> throwError $ FunAlreadyDeclared id p
+
+checkTopFun :: TopDef -> TCMonad ()
+checkTopFun = undefined
+
+checkEveryTopFun :: [TopDef] -> TCMonad ()
+checkEveryTopFun fundefs = do
+    mapM_ funToEnv fundefs
+    mapM_ checkTopFun fundefs
+
+check :: Program -> IO (String, Bool)
+check (Program _ fundefs) = do
+    let runS = runStateT (checkEveryTopFun fundefs) emptyState
+    result <- runExceptT runS
+    case result of
+        Left err -> return (errMsg err, True)
+        Right _ -> return ("", False)
